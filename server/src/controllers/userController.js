b790ab44a5e101f7b0e1421956b3381ccd592cd2
SumitTr96 <sumitkr9199@gmail.com>
2026-02-07 13:24:14 +0530

    initial

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -42,3 +42,47 @@ exports.signup = async(req,res)=>{
         res.status(500).json({message:"Signup failed"})
     }
 }
+
+// SIGN IN
+
+exports.login = async(req,res)=>{
+    try {
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user){
+        res.status(400).json({message:"User not available"});
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        res.status(401).json({message:"Invalid password"})
+    }
+    const token = generateToken(user._id);
+
+    res.cookie("token",token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:"strict",
+        maxAge: 24*60*60*1000
+    })
+
+    res.status(401).json({message:"Login successfull",
+        user:{
+            id:user._id,
+            name:user.name,
+            email:user.email
+        }
+    })
+}catch(error){
+    res.status(500).json({message:"Login Failed"})
+}
+}
+
+// LOGOUT
+
+exports.logout=(req,res)=>{
+    res.clearCookie("token");
+    res.json({message:"Logout successfull"});
+}
